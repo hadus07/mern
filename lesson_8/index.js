50cb02f2742e222cce7ed25093a61f08f90aa1dd
Ibrohim Bahromov <hadus_bi@mail.ru>
2020-01-16 20:57:04 +0500

    Lesson 9

---
+++ lesson_8/index.js
@@ -0,0 +1,17 @@
+let ages1 = [12, 13]
+let ages2 = [14, 15]
+let tmp = ages1 - ages2
+console.log(tmp)
+
+// let ages2 = ages.concat([15, 16])
+// let ages2 = [...ages, ...[15, 16], 17]
+
+// console.log(ages)
+// console.log(ages2)
+
+// let ages2 = [...ages]
+
+// ages2[2] = 14
+
+// console.log(ages2[2])
+// console.log(ages[2])
---
+++ lesson_9/index.js
@@ -0,0 +1,2 @@
+let num = '13.4273593247'
+console.log(Number.parseFloat(num))

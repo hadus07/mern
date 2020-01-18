b3bdb148edd6d8378801404636bcdaf07ffbd8ea
Ibrohim Bahromov <hadus_bi@mail.ru>
2020-01-18 20:55:51 +0500

    Lesson 10

---
+++ lesson_10/index.js
@@ -0,0 +1,59 @@
+let digit = prompt('Yoshingiz:')
+digit = parseInt(digit)
+
+switch (digit) {
+    case 0: 
+        console.log('Zero')
+        break
+    case 1:
+        console.log('One')
+        break
+    case 2:
+        console.log('Two')
+        break
+    case 3:
+        console.log('Three')
+        break
+    case 4: 
+        console.log('Four')
+        break
+    case 5:
+        console.log('Five')
+        break
+    case 6:
+        console.log('Six')
+        break
+    case 7:
+        console.log('Seven')
+        break
+    case 8:
+        console.log('Eight')
+        break
+    case 9:
+        console.log('Nine')
+        break
+    default:
+        console.log('Raqam kiriting')
+}
+
+// if (digit == 0) {
+//     console.log('Zero')
+// } else if (digit == 1) {
+//     console.log('One')
+// } else if (digit == 2) {
+//     console.log('Two')
+// } else if (digit == 3) {
+//     console.log('Three')
+// } else if (digit == 4) {
+//     console.log('Four')
+// } else if (digit == 5) {
+//     console.log('Five')
+// } else if (digit == 6) {
+//     console.log('Six')
+// } else if (digit == 7) {
+//     console.log('Seven')
+// } else if (digit == 8) {
+//     console.log('Eight')
+// } else if (digit == 9) {
+//     console.log('Nine')
+// }
